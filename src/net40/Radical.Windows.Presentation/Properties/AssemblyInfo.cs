4c8a1a4a94b685c3b10185ef86f6345257fe8b0f
Mauro Servienti <mauro@topics.it>
2016-03-26 11:15:28 +0100

    add GitVersionTask and remove stuff from AssemblyInfo

--- src/net40/Radical.Windows.Presentation/Properties/AssemblyInfo.cs
+++ src/net40/Radical.Windows.Presentation/Properties/AssemblyInfo.cs
@@ -36,7 +36,3 @@ using System.Runtime.InteropServices;
 //    //(used if a resource is not found in the page, 
 //    // app, or any theme specific resource dictionaries)
 //)]
-
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
